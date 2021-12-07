78fd33f73d6c7b01a742706fcf09c2035f716b38
bruno <bruno.gouveia@wipro.com>
2021-12-07 18:41:51 -0300

    added cors policy

--- queue_manager/settings.py
+++ queue_manager/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'branches',
     'kitchens',
     'orders',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders'
 
 ]
 
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'queue_manager.urls'
@@ -86,6 +89,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 
 # Password validation
