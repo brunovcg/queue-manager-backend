88ed79a64105e8ce977cb11117ff70dd484341a5
bruno <bruno.gouveia@wipro.com>
2021-12-10 23:54:15 -0300

    Imagefield to filefield

--- kitchens/models.py
+++ kitchens/models.py
@@ -8,7 +8,7 @@ def upload_to(instance, filename):
 class Kitchens (models.Model):
 
     code = models.CharField(max_length=255, unique=True)
-    image = models.ImageField(upload_to=upload_to, default="default.jpg", blank=True)
+    image = models.FileField(upload_to=upload_to, default="default.jpg", blank=True)
     label = models.CharField(max_length=255,blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="kitchens", blank=True, null=True)
     branch = models.ForeignKey('branches.Branches', on_delete=models.CASCADE, related_name="kitchens",blank=True)
